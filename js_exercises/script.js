bc8efbf3191abed57758536d364ba46538e4909d
juliannascruggs <julianna@scrug.gs>
2014-10-19 14:07:50 -0500

    Added three new JS exercises.

---
+++ js_exercises/script.js
@@ -0,0 +1,19 @@
+//Create the needed variables: array, s for sum, p for product and i for counter in for loop
+var funds = [5, 10, 17, 1];
+var s = 0;
+var p = 0;
+var i = 0;
+for (i = 0; i < funds.length; i++) {
+	s += i;
+	if (i ) {
+		console.log("i is equal to " + i);
+	}else{
+//		console.log("the sum is " + s);
+
+	}
+}
+// Create a for loop that will traverse through the data and either add and multiply the numbers
+//together. Make sure each is assigned to the correct variable name specified above.
+
+
+//Print the results to the console.
